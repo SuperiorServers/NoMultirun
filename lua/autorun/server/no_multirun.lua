dde160daca1a26b6cfdc2a052635b0d3c7d34574
Austin Thomas <a.thomas@thed3vine.net>
2017-01-01 23:31:06 -0600

    tmysql3 compatibility

--- lua/autorun/server/no_multirun.lua
+++ lua/autorun/server/no_multirun.lua
@@ -1,27 +1,11 @@
-nomr = nomr or {
-	serverid = util.CRC(GetConVarString('ip') .. ':' .. GetConVarString('hostport'))
-}
+local serverid = util.CRC(GetConVarString('ip') .. ':' .. GetConVarString('hostport'))
 
-include 'dash_mysql_wrapper.lua'
+hook.Add('Initialize', 'nomr.Initialize', function()
+	tmysql.query('DELETE FROM sessions WHERE server = ' .. serverid .. ';')
+end)
 
-
-nomr.db = nomr.db or nomr.mysql('HOSTNAME', 'USERNAME', 'PASSWORD', 'DATABASE', 3306)
-
-nomr.db:Query [[
-	CREATE TABLE IF NOT EXISTS `sessions`(  
-		`steamid64` BIGINT(20) NOT NULL,
-		`time` INT NOT NULL,
-		`server` BIGINT(20) NOT NULL,
-		PRIMARY KEY (`steamid64`)
-	);
-]]
-
-nomr.db:Query('DELETE FROM sessions WHERE server = ' .. nomr.serverid .. ';')
-
-local checksession 	= nomr.db:Prepare('SELECT steamid64, server FROM sessions WHERE steamid64=? AND time >= (UNIX_TIMESTAMP() - 0.5) AND server != ' .. nomr.serverid .. ';')
-local updatesession = nomr.db:Prepare('REPLACE INTO sessions(steamid64, time, server) VALUES(?, UNIX_TIMESTAMP(), ' .. nomr.serverid .. ');')
 hook.Add('CheckPassword', 'nomr.CheckPassword', function(steamid64)
-	checksession:Run(steamid64, function(data)
+	tmysql.query('SELECT steamid64, server FROM sessions WHERE steamid64=' .. tmysql.escape(steamid64) .. ' AND time >= (UNIX_TIMESTAMP() - 0.5) AND server != ' .. serverid .. ';', function(data)
 		if (#data > 0) then
 			game.KickID(util.SteamIDFrom64(steamid64), 'Active session on another server detected') -- if we create your session here you wont be able to join other servers if you lose connection before you're authed
 		end
@@ -29,23 +13,21 @@ hook.Add('CheckPassword', 'nomr.CheckPassword', function(steamid64)
 end)
 
 hook.Add('PlayerAuthed', 'nomr.PlayerAuthed', function(pl)
-	checksession:Run(pl:SteamID64(), function(data)
+	tmysql.query('SELECT steamid64, server FROM sessions WHERE steamid64=' .. tmysql.escape(pl:SteamID64()) .. ' AND time >= (UNIX_TIMESTAMP() - 0.5) AND server != ' .. serverid .. ';', function(data)
 		if IsValid(pl) then
 			if (#data > 0) then
 				game.KickID(pl:SteamID(), 'Active session on another server detected') -- You tried to join before your session was made
 			else
-				updatesession:Run(pl:SteamID64())
+				tmysql.query('REPLACE INTO sessions(steamid64, time, server) VALUES(' .. tmysql.escape(pl:SteamID64()) .. ', UNIX_TIMESTAMP(), ' .. serverid .. ');')
 			end
 		end
 	end)
 end)
 
-local deletesession = nomr.db:Prepare('DELETE FROM sessions WHERE steamid64=? AND server = ' .. nomr.serverid .. ';')
 hook.Add('PlayerDisconnected', 'nomr.PlayerDisconnected', function(pl)
-	deletesession:Run(pl:SteamID64())
+	tmysql.query('DELETE FROM sessions WHERE steamid64=' .. tmysql.escape(pl:SteamID64()) .. ' AND server = ' .. serverid .. ';')
 end)
 
-local updatesessions = nomr.db:Prepare('UPDATE sessions SET time = UNIX_TIMESTAMP() WHERE server = ' .. nomr.serverid .. ';')
 timer.Create('nomr.UpdateSessions', 0.25, 0, function()
-	updatesessions:Run()
+	tmysql.query('UPDATE sessions SET time = UNIX_TIMESTAMP() WHERE server = ' .. serverid .. ';')
 end)
